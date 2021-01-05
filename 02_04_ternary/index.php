f1932d4b72d07e739fcd5628b989cc92a868369f
Asim Ghaffar <asim.ghaffar@gmail.com>
2021-01-06 03:52:44 +0500

    creating if/else statements

---
+++ 02_04_ternary/index.php
@@ -0,0 +1,11 @@
+<?php
+    if (5 < 4) {
+        echo '5 is bigger than 4<br>';
+    } else if (4 > 3) {
+        echo '4 is bigger than 3<br>';
+    } else {
+        echo 'nothing is true<br>';
+
+    }
+    echo "out of ...";
+?>
