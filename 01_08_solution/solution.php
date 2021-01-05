b66a60e0ad4888b353a6e05343a7d1a5ef9b0471
Asim Ghaffar <asim.ghaffar@gmail.com>
2021-01-05 23:44:34 +0500

    My solution

--- 01_08_solution/solution.php
+++ 01_08_solution/solution.php
@@ -4,4 +4,9 @@ $city = "NYC";
 $movie = "Star Wars"; // Italics
 $friend = "George"; // Bold
 $candy = "Sour Patch kids";
+
+
+
+echo "<h1>$name</h1> lives in $city. She decided to watch <i>$movie</i> 
+with <b>$friend</b> who like to eat $candy";
 ?>
