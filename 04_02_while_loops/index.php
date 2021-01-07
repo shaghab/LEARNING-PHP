3650657a93c68b76a8f68557164e82aa6536771a
Asim Ghaffar <asim.ghaffar@gmail.com>
2021-01-07 23:27:51 +0500

    printing array using a loop

--- 04_02_while_loops/index.php
+++ 04_02_while_loops/index.php
@@ -1,14 +1,20 @@
 <?php
-    $i = 0;
+    $i = 10;
 
     do {
         
-        echo '<p>'.$i++.'</p>';
+        //echo '<p>'.$i++.'</p>';
 
     } while ($i < 10);
 
     for($i = 0 ; $i < 10; $i++) {
-        echo '<p><b>'.$i.'</b></p>';
+        //echo '<p><b>'.$i.'</b></p>';
+    }
+
+    $colors = array ('red', 'blue', 'green' , 'yellow', 'white');
+
+    for($i = 0 ; $i < sizeof($colors); $i++) {
+        echo '<p><b>'.$colors[$i].'</b></p>';
     }
 
 ?>
