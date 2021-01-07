3d04b76ac613f2ceb1f2425384c493ddfc5762cf
Asim Ghaffar <asim.ghaffar@gmail.com>
2021-01-07 23:37:08 +0500

    foreach for associative array

--- 04_04_foreach_loops/index.php
+++ 04_04_foreach_loops/index.php
@@ -5,5 +5,15 @@
         echo $clr.'<br>';
     }
 
+    $home_towns = array (
+        'asim' => 'islamabad',
+        'usman' => 'lahore',
+        'umair' => 'karachi',
+    );
+
+    foreach($home_towns as $key => $value) {
+        echo "<p>$key lives in $value</p>";
+    }
+
 
 ?>
