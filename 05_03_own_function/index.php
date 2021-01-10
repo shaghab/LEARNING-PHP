fef425851e786312cb5905a5fbe537c34a7ff0a0
Asim Ghaffar <asim.ghaffar@gmail.com>
2021-01-10 21:41:18 +0500

    handled use case for case difference and also for spaces..

--- 05_03_own_function/index.php
+++ 05_03_own_function/index.php
@@ -1,14 +1,18 @@
 <?php
 
     function is_plaindrome( $string) {
+
+        $string = strtolower($string);
+        $string = str_replace (' ', '', $string);
+
         $reverse = strrev($string);
         return $string == $reverse;
     }
 
-    echo is_plaindrome('Hello').'<br>';
-    echo is_plaindrome('ala').'<br>';
-    echo is_plaindrome('alla').'<br>';
-    echo is_plaindrome('welcome').'<br>';
- 
-
+    echo '<pre>';
+    echo var_dump( is_plaindrome('Hello'));
+    echo var_dump(is_plaindrome('ala'));
+    echo var_dump(is_plaindrome('alla'));
+    echo var_dump(is_plaindrome('welcome'));
+    echo '</pre>';
 ?>
