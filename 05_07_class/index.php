721de873112fe6382af4bb4e37725d8dffdded90
Asim Ghaffar <asim.ghaffar@gmail.com>
2021-01-17 20:34:57 +0500

    fixing errors and testing class

--- 05_07_class/index.php
+++ 05_07_class/index.php
@@ -5,31 +5,35 @@
         var $birthday = false;
 
         function __construct ( $name, $age) {
-            $this->$name = $name;
-            $this->$age = $age;
+            $this->name = $name;
+            $this->age = $age;
         }
 
         public function get_name() {
-            return $this->$name;
+            return $this->name;
         }
 
         public function get_age() {
-            return $this->$age;
+            return $this->age;
         }
 
         public function set_name($new_name) {
-            $this->$name = $new_name;
+            $this->name = $new_name;
         }
 
         public function set_birthday ( $b) {
-            $this->$birthday = $b;
+            $this->birthday = $b;
             $this->update_age();
         }
 
         private function update_age() {
-            $this->$age = ($this->$birthday) ? ++$this->age : $this->age;
+            $this->age = ($this->birthday) ? ++$this->age : $this->age;
         }
 
     }
+
+    $rob = new Person ('Rob', 15);
+    echo $rob->get_name();
+    echo $rob->get_age();
 
 ?>
