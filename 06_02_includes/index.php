0ecfaca0b9666da8b3c20657107a0514822202f6
Asim Ghaffar <asim.ghaffar@gmail.com>
2021-01-18 23:57:49 +0500

    nav added

--- 06_02_includes/index.php
+++ 06_02_includes/index.php
@@ -5,6 +5,7 @@
         <title><?php echo $title; ?></title>
     </head>
     <body>
+        <nav><a href="about.php">About</a></nav>
         <h1><?php echo $title; ?></h1>
         <p><?php echo $description; ?></p>
         <small>By: <?php echo $author; ?></small>
